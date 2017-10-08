398be774acfecc912591c0e018ca93c976a74dc0
thang211184 <thang211184@gmail.com>
2017-10-08 11:38:55 -0500

    successfully load crate on boat

---
+++ Assets/CrateController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateController : MonoBehaviour {
+
+	public float thrust;
+	public Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		rb.AddForce(0, -10, 0);
+		//rb.AddForce (1, 0, 0);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Boundary") {
+			return;
+		}
+		gameObject.transform.parent = other.transform;
+		Destroy (rb);
+	}
+}
--- Assets/Scripts/AttachOnContact.cs
+++ Assets/Scripts/AttachOnContact.cs
@@ -3,20 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AttachOnContact : MonoBehaviour {
-	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Boundary") {
-			return;
-		}
-		Debug.Log (other.name);
-		other.transform.parent = gameObject.transform;
-	}
-
-	void OnTriggerExit(Collider other) {
-		if (other.tag == "Boundary") {
-			return;
-		}
-		Debug.Log (other.name);
-		other.transform.parent = null;
-	}
+	
 
 }
