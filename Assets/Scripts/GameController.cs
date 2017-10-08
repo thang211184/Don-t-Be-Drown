950533d2a277e6c8683871522bd449498f25c097
thang211184 <thang211184@gmail.com>
2017-10-08 09:51:52 -0500

    updates

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject crate;
+	public Transform droppingPoint;
+	public int crateCount;
+	public float dropRate;
+
+	void Start(){
+		StartCoroutine (SpawnWave ());
+	}
+	IEnumerator SpawnWave(){
+		yield return new WaitForSeconds (dropRate);
+		for (int i = 0; i < crateCount; i++) {
+			Instantiate (crate, droppingPoint.position, droppingPoint.rotation);
+			yield return new WaitForSeconds (dropRate);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary{
+	public float xMin, Xmax;
+}
+
+public class PlayerController : MonoBehaviour {
+	public Rigidbody rb;
+	public float speed;
+	public Boundary boundary;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		Vector2 move = new Vector2 (moveHorizontal, 0.0f);
+		rb.velocity = move*speed;
+
+		rb.position = new Vector2 (
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.Xmax), 
+			-5.42f
+		);
+	}
+}
