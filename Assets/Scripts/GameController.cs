5557acd580ee8fea48b53b20e55ec2c4c117f28b
kkhanipov <32584968+kkhanipov@users.noreply.github.com>
2017-10-17 13:02:23 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 4a05d3079d2f5e0bcaa5ef4b0e380d06375c2635, reversing
    changes made to 53712bba0f4eabcdb405722b106f30045a6105ad.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,73 +4,47 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class GameController : MonoBehaviour
-{
-    // Crates
-    public GameObject crate;
-    public GameObject boat;
-    public GameObject net;
-    public Transform droppingPoint;
-    public int crateCount;
-    public float dropRate;
-    bool net_drop;
+public class GameController : MonoBehaviour {
+	// Crates
+	public GameObject crate;
+	public GameObject boat;
+	public Transform droppingPoint;
+	public int crateCount;
+	public float dropRate;
 
-    // Time
-    public float startingTime;
-    public GUIText theText;
+	// Time
+	public float startingTime;
+	public GUIText theText;
 
-    // Score
-    public GUIText scoreText;
-    public int score;
+	// Score
+	public GUIText scoreText;
+	public int score;
 
-    // Hint
-    public GUIText hintText;
+	// Hint
+	public GUIText hintText;
 
-    // Total crates on boat
-    GameObject[] totalCrates;
+	// Total crates on boat
+	GameObject[] totalCrates ;
 
-    void Start()
-    {
-        score = 0;
-        StartCoroutine(SpawnWave());
-        net_drop = false;
-        scoreUpdate();
-    }
+	void Start(){
+		score = 0;
+		StartCoroutine (SpawnWave ());
+	}
 
-    // Create crates from above
-    IEnumerator SpawnWave()
-    {
-        // Wait for few second before start dropping crates
-        yield return new WaitForSeconds(dropRate);
+	// Create crates from above
+	IEnumerator SpawnWave(){
+		// Wait for few second before start dropping crates
+		yield return new WaitForSeconds (dropRate);
 
-        // Thuan, you can add sound of dropping crate here
+		// Thuan, you can add sound of dropping crate here
 
-        for (int i = 0; i < crateCount; i++)
-        {
-            // Create crate and drop it
-            float x = Random.Range(-3, 3);
+		for (int i = 0; i < crateCount; i++) {
+			// Create crate and drop it
+			float x = Random.Range(-3, 3);
             float y = 4.96f;
             float z = 0;
             Vector3 pos = new Vector3(x, y, z);
             droppingPoint.position = pos;
-<<<<<<< HEAD
-            Instantiate(crate, droppingPoint.position, droppingPoint.rotation);
-            yield return new WaitForSeconds(dropRate);
-        }
-    }
-
-    // Add score to every catched crate
-    public void AddScore(int newScoreValue)
-    {
-        score += newScoreValue;
-        scoreUpdate();
-    }
-
-    // Add crates to the list to destroy later when unload them at shelter station
-    /*public void AddCrate(GameObject newCrate){
-		totalCrates.Add (newCrate);
-	}*/
-=======
 			Instantiate (crate, droppingPoint.position, droppingPoint.rotation);
 			yield return new WaitForSeconds (dropRate);
 		}
@@ -85,64 +59,19 @@ public class GameController : MonoBehaviour
 	void scoreUpdate(){
 		scoreText.text = "Score :" + score.ToString();
 	}
->>>>>>> 16a7f15a4292c6fc378e7bfc614552a47181996e
 
-    void scoreUpdate()
-    {
-        scoreText.text = "Coins :" + score;
-    }
-
-    void Update()
-    {
-        // Time counting
-        startingTime -= Time.deltaTime;
-        theText.text = Mathf.Round(startingTime).ToString();
+	void Update(){
+		// Time counting
+		startingTime -= Time.deltaTime;
+		theText.text = Mathf.Round(startingTime).ToString();
 
 
-        // Unload crates
-        if (Input.GetKey(KeyCode.Z))
-        {
-            if (boat.transform.position.x < -7.5)
-            {
-                /*for (int i = 0; i <= totalCrates.Count; i++) {
+		// Unload crates
+		if (Input.GetKey(KeyCode.Z)) {
+			if (boat.transform.position.x < -7.5) {
+				/*for (int i = 0; i <= totalCrates.Count; i++) {
 					Destroy (totalCrates [i]);
 				}*/
-<<<<<<< HEAD
-                totalCrates = GameObject.FindGameObjectsWithTag("Crate");
-                for (int i = 0; i < totalCrates.Length; i++)
-                {
-                    AddScore(10);
-                    Destroy(totalCrates[i]);
-                }
-                //hintText.text = "Thank you very muchhhhhhhhhhhhh";
-                net_drop = false;
-            }
-            else
-            {
-                hintText.text = "please go to shelter to unload crates";
-            }
-            return;
-        }
-
-        if (Input.GetKey(KeyCode.X))
-        {
-            if (!net_drop)
-            {
-                float x = 0;
-                float y = 4.96f;
-                float z = 0;
-                Vector3 pos = new Vector3(x, y, z);
-                droppingPoint.position = pos;
-                Instantiate(net, droppingPoint.position, droppingPoint.rotation);
-                net_drop = true;
-                return;
-            }
-        }
-        // Detroy crate when go out boundary
-    }
-
- 
-=======
 				totalCrates = GameObject.FindGameObjectsWithTag("Crate");
 				for (int i = 0; i < totalCrates.Length; i++) {
 					Destroy (totalCrates [i]);
@@ -167,5 +96,4 @@ public class GameController : MonoBehaviour
 	}
 
 
->>>>>>> 16a7f15a4292c6fc378e7bfc614552a47181996e
 }
--- Assets/Scripts/alligator_move.cs
+++ Assets/Scripts/alligator_move.cs
@@ -2,50 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class alligator_move : MonoBehaviour
-{
+public class alligator_move : MonoBehaviour {
     public Transform target;//set target from inspector instead of looking in Update
     public float speed;
 
     // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
 
         target = GameObject.FindWithTag("Boat").transform;
 
         float step = speed * Time.deltaTime;
-        Vector3 offset = new Vector3(0, -.5f, 0);
+        Vector3 offset = new Vector3(0, -1.7f, 0);
         Vector3 targetHeading = target.position + offset;
-        Vector3 targetDirection = targetHeading.normalized;
+        //Vector3 targetDirection = targetHeading.normalized;
 
         transform.position = Vector3.MoveTowards(transform.position, targetHeading, step);
 
-        //   Vector3 position = new Vector3(Random.Range(-1, 1), 0, 0);
-        //  transform.Translate(Vector3.MoveTowards(Vector3.curr* Time.deltaTime, Camera.main.transform);
+        //Vector3 position = new Vector3(Random.Range(-1, 1), 0, 0);
+        //transform.Translate(Vector3.MoveTowards(Vector3.curr* Time.deltaTime, Camera.main.transform);
     }
-
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-       // if (other.gameObject.CompareTag("Net"))
-        //{
-            Invoke("Reappear", 15);
-            gameObject.SetActive(false);
-            other.gameObject.SetActive(false);
-       // }
-    }
-
-        void Reappear()
-        {
-        Vector3 position = new Vector3(5.29f,-2.81f,0);
-        transform.position = position;
-        gameObject.SetActive(true); 
-        }
-
 }
