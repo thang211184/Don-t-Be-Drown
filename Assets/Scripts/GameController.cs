ff0a6c3c32aecc4b58f4ab6ddfaef0abe8e73fc1
kkhanipov <32584968+kkhanipov@users.noreply.github.com>
2017-10-17 10:06:35 -0500

    Net v 0.1

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,92 +2,132 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
-	// Crates
-	public GameObject crate;
-	public GameObject boat;
-	public Transform droppingPoint;
-	public int crateCount;
-	public float dropRate;
+public class GameController : MonoBehaviour
+{
+    // Crates
+    public GameObject crate;
+    public GameObject boat;
+    public GameObject net;
+    public Transform droppingPoint;
+    public int crateCount;
+    public float dropRate;
+    bool net_drop;
 
-	// Time
-	public float startingTime;
-	public GUIText theText;
+    // Time
+    public float startingTime;
+    public GUIText theText;
 
-	// Score
-	public GUIText scoreText;
-	public int score;
+    // Score
+    public GUIText scoreText;
+    public int score;
 
-	// Hint
-	public GUIText hintText;
+    // Hint
+    public GUIText hintText;
 
-	// Total crates on boat
-	GameObject[] totalCrates ;
+    // Total crates on boat
+    GameObject[] totalCrates;
 
-	void Start(){
-		score = 0;
-		StartCoroutine (SpawnWave ());
-	}
+    void Start()
+    {
+        score = 0;
+        StartCoroutine(SpawnWave());
+        StartCoroutine(Net_drop());
+        net_drop = false;
+    }
 
-	// Create crates from above
-	IEnumerator SpawnWave(){
-		// Wait for few second before start dropping crates
-		yield return new WaitForSeconds (dropRate);
+    // Create crates from above
+    IEnumerator SpawnWave()
+    {
+        // Wait for few second before start dropping crates
+        yield return new WaitForSeconds(dropRate);
 
-		// Thuan, you can add sound of dropping crate here
+        // Thuan, you can add sound of dropping crate here
 
-		for (int i = 0; i < crateCount; i++) {
-			// Create crate and drop it
-			float x = Random.Range(-3, 3);
+        for (int i = 0; i < crateCount; i++)
+        {
+            // Create crate and drop it
+            float x = Random.Range(-3, 3);
             float y = 4.96f;
             float z = 0;
             Vector3 pos = new Vector3(x, y, z);
             droppingPoint.position = pos;
-			Instantiate (crate, droppingPoint.position, droppingPoint.rotation);
-			yield return new WaitForSeconds (dropRate);
-		}
-	}
+            Instantiate(crate, droppingPoint.position, droppingPoint.rotation);
+            yield return new WaitForSeconds(dropRate);
+        }
+    }
 
-	// Add score to every catched crate
-	public void AddScore(int newScoreValue){
-		score += newScoreValue;
-		scoreUpdate ();
-	}
+    // Add score to every catched crate
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        scoreUpdate();
+    }
 
-	// Add crates to the list to destroy later when unload them at shelter station
-	/*public void AddCrate(GameObject newCrate){
+    // Add crates to the list to destroy later when unload them at shelter station
+    /*public void AddCrate(GameObject newCrate){
 		totalCrates.Add (newCrate);
 	}*/
-		
-	void scoreUpdate(){
-		scoreText.text = "Score :" + score + " Crate: " + (totalCrates.Length);
-	}
 
-	void Update(){
-		// Time counting
-		startingTime -= Time.deltaTime;
-		theText.text = Mathf.Round(startingTime).ToString();
+    void scoreUpdate()
+    {
+        scoreText.text = "Score :" + score + " Crate: " + (totalCrates.Length);
+    }
+
+    void Update()
+    {
+        // Time counting
+        startingTime -= Time.deltaTime;
+        theText.text = Mathf.Round(startingTime).ToString();
 
 
-		// Unload crates
-		if (Input.GetKey(KeyCode.Z)) {
-			if (boat.transform.position.x < -7.5) {
-				/*for (int i = 0; i <= totalCrates.Count; i++) {
+        // Unload crates
+        if (Input.GetKey(KeyCode.Z))
+        {
+            if (boat.transform.position.x < -7.5)
+            {
+                /*for (int i = 0; i <= totalCrates.Count; i++) {
 					Destroy (totalCrates [i]);
 				}*/
-				totalCrates = GameObject.FindGameObjectsWithTag("Crate");
-				for (int i = 0; i < totalCrates.Length; i++) {
-					Destroy (totalCrates [i]);
-				}
-				//hintText.text = "Thank you very muchhhhhhhhhhhhh";
-			} else {
-				hintText.text = "please go to shelter to unload crates";
-			}
-				return;
-		}
+                totalCrates = GameObject.FindGameObjectsWithTag("Crate");
+                for (int i = 0; i < totalCrates.Length; i++)
+                {
+                    Destroy(totalCrates[i]);
+                }
+                //hintText.text = "Thank you very muchhhhhhhhhhhhh";
+            }
+            else
+            {
+                hintText.text = "please go to shelter to unload crates";
+            }
+            return;
+        }
 
-		// Detroy crate when go out boundary
-	}
+        if (Input.GetKey(KeyCode.X))
+        {
+            if (!net_drop)
+            {
+                float x = 0;
+                float y = 4.96f;
+                float z = 0;
+                Vector3 pos = new Vector3(x, y, z);
+                droppingPoint.position = pos;
+                Instantiate(net, droppingPoint.position, droppingPoint.rotation);
+                net_drop = true;
+                return;
+            }
+        }
+        // Detroy crate when go out boundary
+    }
 
+    IEnumerator Net_drop()
+    {
+        // Wait for few second before start dropping crates
+        if (net_drop)
+        {
+            net_drop = false;
+            yield return new WaitForSeconds(2);
+            
+        }
 
+    }
 }
